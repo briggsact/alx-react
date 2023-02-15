7a7f86632ae749c7ce78f9929d68645bb66e410b
briggsact <99441221+briggsact@users.noreply.github.com>
2023-02-15 20:53:40 +0100

    my commit

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,5 +1,9 @@
-import { getIn, fromJS } from 'immutable';
+/* function should return the value of the object at the defined path */
+import { fromJS } from 'immutable';
 
 export default function accessImmutableObject(object, array) {
-	return getIn(fromJS(object), array);
+  // make the object immutable
+  const ImmutableObject = fromJS(object);
+  // checks and returns any keys in the list can be found in ImmutableOject
+  return ImmutableObject.getIn(array, undefined);
 }
